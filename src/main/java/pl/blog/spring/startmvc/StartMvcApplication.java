e7bf9ccd533a959e2d4535123845ece0153133e7
Kamil Klimek <Kamil.Klimek@ttpsc.pl>
2019-12-29 15:45:24 +0100

    Initial commit

---
+++ src/main/java/pl/blog/spring/startmvc/StartMvcApplication.java
@@ -0,0 +1,13 @@
+package pl.blog.spring.startmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/blog/spring/startmvc/StartMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.blog.spring.startmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
